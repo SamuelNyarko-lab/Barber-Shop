571d72a104441ee49d5055914ec9e95504a3f36b
Samuel Nyarko <samuelnyarko003@gmail.com>
2023-01-29 04:34:53 +0000

    Start Project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/splashscreen.dart';
+
+void main() {
+  runApp(const BarberShop());
+}
+
+class BarberShop extends StatelessWidget {
+  const BarberShop({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Barber Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
